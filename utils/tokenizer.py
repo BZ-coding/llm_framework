6ac0bf92054f7a606083ad5b48a4518ab6b37643
zhangshengdong29 <435878393@qq.com>
2024-01-12 19:22:23 +0800

    Update tokenizer.py

--- utils/tokenizer.py
+++ utils/tokenizer.py
@@ -3,7 +3,7 @@ import logging
 from transformers import AutoTokenizer
 
 
-def get_tokenizer(tokenizer_path, pad_token_id=0):
+def get_tokenizer(tokenizer_path, pad_token_id=0, use_fast=True):
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
     tokenizer.pad_token_id = pad_token_id  # 0, unk. we want this to be different from the eos token
     logging.info(f"tokenizer.pad_token_id:{tokenizer.pad_token_id} tokenizer.pad_token:{tokenizer.pad_token}")
