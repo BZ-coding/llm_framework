94a1f64100182ce245343ff8be35677feab7eb49
zhangshengdong29 <435878393@qq.com>
2024-01-30 11:47:45 +0800

    Update train_launch.sh

--- train_launch.sh
+++ train_launch.sh
@@ -1,9 +1,8 @@
-deepspeed train_with_trainer.py
 #accelerate config --config_file accelerate_deepspeed_config.yaml
 
+deepspeed train_with_trainer.py | tee train.log
+#accelerate launch --config_file accelerate_deepspeed_config.yaml train_with_trainer.py | tee train.log
 
-#accelerate launch --config_file accelerate_deepspeed_config.yaml train_with_trainer.py
-#python train_with_megatron.py
-#accelerate launch --config_file accelerate_megatron_config.yaml train_with_trainer.py
+#accelerate launch --config_file accelerate_megatron_config.yaml train_with_megatron.py | tee train.log
 
-#python train_with_trainer.py
+#python train_with_trainer.py | tee train.log
