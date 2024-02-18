7375b2bb5a5f82077a641416b792d6c4a252a3f3
zhangshengdong29 <435878393@qq.com>
2024-02-18 18:31:00 +0800

    Update log.py

--- tools/log.py
+++ tools/log.py
@@ -7,7 +7,9 @@ transformers.utils.logging.enable_default_handler()
 transformers.utils.logging.enable_explicit_format()
 
 
-def get_logger(log_level, logger_log_level=logging.INFO, logger=None, log_file=None):  # todo: add accelerate
+def get_logger(log_level, logger_log_level=None, logger=None):
+    if logger_log_level is None:
+        logger_log_level = log_level
     if logger is None:
         logger = logging.getLogger("__main__")
     console_handler = logging.StreamHandler()
@@ -15,9 +17,4 @@ def get_logger(log_level, logger_log_level=logging.INFO, logger=None, log_file=N
     console_handler.setLevel(logger_log_level)
     datasets.utils.logging.set_verbosity(log_level)
     transformers.utils.logging.set_verbosity(log_level)
-    if log_file:
-        file_handler = logging.FileHandler(log_file, mode='w')
-        file_handler.setLevel(logger_log_level)
-        logger.addHandler(file_handler)
-        transformers.utils.logging.add_handler(file_handler)
     return logger
